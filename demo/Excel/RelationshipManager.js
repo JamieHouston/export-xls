6b5497c74be7315a93f119df43af19491dd81503
Jamie Houston <foresterh@gmail.com>
2013-10-23 11:13:27 -0700

    initial commit

---
+++ demo/Excel/RelationshipManager.js
@@ -0,0 +1,54 @@
+"use strict";
+/**
+ * @module Excel/RelationshipManager
+ */
+define(['underscore', './util', './Paths'], function (_, util, Paths) {
+    var RelationshipManager = function () {
+        this.relations = {};
+        this.lastId = 1;
+    };
+    
+    _.uniqueId('rId'); //priming
+    
+    _.extend(RelationshipManager.prototype, {
+        
+        importData: function (data) {
+            this.relations = data.relations;
+            this.lastId = data.lastId;
+        },
+        exportData: function () {
+            return {
+                relations: this.relations,
+                lastId: this.lastId
+            };
+        },
+        
+        addRelation: function (object, type) {
+            this.relations[object.id] = {
+                id: 'rId' + this.lastId++,
+                schema: util.schemas[type]
+            };
+        },
+        
+        getRelationshipId: function (object) {
+            return this.relations[object.id].id
+        },
+		
+        toXML: function () {
+            var doc = util.createXmlDoc(util.schemas.relationshipPackage, 'Relationships');
+            var relationships = doc.documentElement;
+            
+            _.each(this.relations, function (data, id) {
+                var relationship = util.createElement(doc, 'Relationship', [
+                    ['Id', data.id],
+                    ['Type', data.schema],
+                    ['Target', Paths[id]]
+                ]);
+                relationships.appendChild(relationship);
+            });
+            return doc;
+        }
+    });
+    
+    return RelationshipManager;
+});
